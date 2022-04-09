606307663007ae6a40d94cb0b26ec783cdf5b24a
Nafis Khan <49691160+nafisK@users.noreply.github.com>
2022-04-08 22:05:00 -0400

    added nafis project file

---
+++ nafis_project_1.scm
@@ -0,0 +1,14 @@
+#lang racket
+
+
+;; isPrime()
+(define (prime? n)
+  (define (F n i) "helper"
+    (cond ((< n (* i i)) #t)
+          ((zero? (remainder n i)) #f)
+          (else
+           (F n (+ i 1)))))
+ "primality test"
+ (cond ((< n 2) #f)
+     (else
+      (F n 2))))
