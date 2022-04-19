e1e6034517729f7e795f2061b70e903e8d1a4f94
Deepankar Chakraborty <52084764+deepankarck2@users.noreply.github.com>
2022-04-19 14:06:25 -0400

    Organized len func

--- Project_1.scm
+++ Project_1.scm
@@ -175,9 +175,10 @@
 (define (len n)
   (define (get-len n length-so-far)
     (cond ((= n 1) length-so-far)
-          (else (let ((kth (ref n length-so-far)) (currPrime (nth-Prime? length-so-far)))
-                  (get-len (/ n (expt currPrime kth)) (+ 1 length-so-far))))))
-
+          (else (let ((kth (ref n length-so-far))
+                      (currPrime (nth-Prime? length-so-far)))
+                  (get-len (/ n (expt currPrime kth))
+                           (+ 1 length-so-far))))))
   (get-len n 0))
 
 
